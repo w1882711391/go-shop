dd79d103b0c6996da9b87be612556189ab530a45
w1882711391 <1182711391@qq.com>
2023-07-17 14:51:12 +0800

    new product

--- dao/mysql.go
+++ dao/mysql.go
@@ -1,1 +1,19 @@
 package dao
+
+import (
+	"errors"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func MysqlInit() error {
+	dsn := "root:123456@tcp(localhost:3306)/xiaowang?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		return errors.New("数据库连接失败")
+	}
+	DB = db
+	return nil
+}
--- model/product.go
+++ model/product.go
@@ -1,1 +1,18 @@
 package model
+
+import "gorm.io/gorm"
+
+// Product 商品结构体
+type Product struct {
+	gorm.Model
+	NickName    string `json:"nick_name"`
+	Price       int    `json:"price"`
+	Description string `json:"description"` //商品描述信息
+	Stock       int16  `json:"stock"`       //库存数量
+}
+
+// CartItem 购物车中商品信息
+type CartItem struct {
+	Product
+	Number int16 `json:"number"` //购买数量
+}
