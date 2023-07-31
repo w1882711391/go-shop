02bbf2ed40e91273504219b4f136a617bc190c6d
w1882711391 <1182711391@qq.com>
2023-07-31 15:59:55 +0800

    增加优雅启停

--- main.go
+++ main.go
@@ -1,23 +1,38 @@
 package main
 
 import (
+	"context"
 	"github.com/sirupsen/logrus"
 	"go_shop/dao"
 	"go_shop/model"
 	"go_shop/route"
+	"os/signal"
+	"syscall"
 )
 
-func main() {
+var listenHttpErrChan = make(chan error)
+
+func init() {
 	dao.MysqlInit()
 	logrus.Info("mysql数据库已启动")
 	dao.RedisInit()
 	logrus.Info("redis数据库已启动")
 	model.AutoMigrate()
 	logrus.Info("数据库表创建成功")
-	app := route.RouterInit()
-	logrus.Info("服务器已启动，监听端口8080")
+}
 
-	if err := app.Listen(":8080"); err != nil {
-		return
+func main() {
+	ctx, cancel := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer cancel()
+	go func() {
+		app := route.RouterInit()
+		listenHttpErrChan <- app.Listen(":8080")
+	}()
+
+	select {
+	case err := <-listenHttpErrChan:
+		logrus.Errorf("http err: %+v\n", err)
+	case <-ctx.Done():
+		logrus.Info("Shutting down gracefully...")
 	}
 }
